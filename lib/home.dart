9784f1aebc8a9bfb37cd28794c4dd76470cb2580
Lab <seoye6618@gmail.com>
2022-05-19 17:42:34 +0900

    [FEAT] 1. bottomNavigationBar only in Challenge page

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:movement/util/size.dart';
 
 class ChallengePage extends StatefulWidget {
   const ChallengePage({Key? key}) : super(key: key);
@@ -10,34 +11,59 @@ class ChallengePage extends StatefulWidget {
 }
 
 class _ChallengePageState extends State<ChallengePage> {
+  int _selectedIndex = 1;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    ChallengePage(),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: SvgPicture.asset("assets/image/challenge.svg"),
+        leading: SvgPicture.asset("assets/image/challenge.svg", color: Colors.white,),
         title: Text("CHALLENGE", style: GoogleFonts.imprima(fontSize: 25)),
       ),
       body: Container(
         color: Colors.white,
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
+          child: ListView(
             children: [
               Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text("도전 중인 챌린지",
                   style: TextStyle(
                     fontSize: 20
                   ),
                   ),
-                  // ListView.builder(
-                  //   itemBuilder: (BuildContext context, int index) {  },
-                  //   scrollDirection: Axis.horizontal,
-                  // ),
-                  Container(
-                    height: 300,
-                    color: Colors.yellow,
-                    child: Center(child: Text("ListView place")),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: Container(
+                      height: getScreenHeight(context)*0.3,
+                      color: Colors.yellow,
+                      child: Center(child: Text("ListView place")),
+                      // ListView.builder(
+                      //   itemBuilder: (BuildContext context, int index) {  },
+                      //   scrollDirection: Axis.horizontal,
+                      // ),
+                    ),
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
@@ -47,10 +73,66 @@ class _ChallengePageState extends State<ChallengePage> {
                   ),
                 ],
               ),
-
+              Divider(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("함께 도전하기",
+                    style: TextStyle(
+                        fontSize: 20
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Container(
+                      height: getScreenHeight(context)*0.3,
+                      color: Colors.yellow,
+                      child: Center(child: Text("ListView place")),
+                      // ListView.builder(
+                      //   itemBuilder: (BuildContext context, int index) {  },
+                      //   scrollDirection: Axis.horizontal,
+                      // ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text("새 그룹 만들기 + "),
+                    ],
+                  ),
+                ],
+              ),
             ],
           ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/image/news.svg",
+              color: (_selectedIndex == 0)? Colors.amber[800] : Colors.black12,
+            ),
+            label: 'News',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/image/challenge.svg",
+              color: (_selectedIndex == 1)? Colors.amber[800] : Colors.black12,
+            ),
+            label: 'Challenge',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/image/account.svg",
+              color: (_selectedIndex == 2)? Colors.amber[800] : Colors.black12,
+            ),
+            label: 'Account',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
--- lib/login.dart
+++ lib/login.dart
@@ -151,3 +151,4 @@ class Authentication extends StatelessWidget {
     );
   }
 }
+
